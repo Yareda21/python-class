d137c73a83a6bb40f960b44479c0b0c8a808930e
kidus <hailukidus360@gmail.com>
2024-08-24 10:25:49 +0300

    the updated code

--- dice_rolling.py
+++ dice_rolling.py
@@ -1,12 +1,17 @@
 #This is the dice rolling simiulator
 import random
 
+
 def Random_number_generator():
         guess = (random.randint(1,6))  
         chances = 2
        
         while chances > 0:
-            guess_2 = int(input("your number please:- "))
+            guess_2 = int(input("your number please from (1-6) :- "))
+            if guess_2 < 1 or guess_2 > 6:
+                 print("pls enter a number between 1 and 6")
+                 continue
+            
             if guess == guess_2:
                 print("YOu got it right")
                 return
@@ -14,12 +19,15 @@ def Random_number_generator():
                 print("your number is too huge")
             else:
                 print("your guess is too low")
-# this is the codes that will happen if you dont get the number.
+
             chances -=1
-# every time you dont get the number it cancel one chance.
+            # every time you dont get the number it cancel one chance.
             if chances > 0:
                 print(f"You have {chances} chances left.")
             else:
                 print(f"You have run out of chances. The number was {guess}.")
-# this the code to excute the random number generator
+
+
+
+#  this the code to excute the random number generator
 Random_number_generator()
