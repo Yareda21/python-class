16566bec3680187889a60b3e0d7ec934a6ab50f2
kidus <hailukidus360@gmail.com>
2024-08-24 09:37:25 +0300

    give explantion to each code

--- dice_rolling.py
+++ dice_rolling.py
@@ -1,3 +1,4 @@
+#This is the dice rolling simiulator
 import random
 
 def Random_number_generator():
@@ -13,10 +14,12 @@ def Random_number_generator():
                 print("your number is too huge")
             else:
                 print("your guess is too low")
+# this is the codes that will happen if you dont get the number.
             chances -=1
-        
+# every time you dont get the number it cancel one chance.
             if chances > 0:
                 print(f"You have {chances} chances left.")
             else:
                 print(f"You have run out of chances. The number was {guess}.")
+# this the code to excute the random number generator
 Random_number_generator()
