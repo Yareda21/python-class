b0a6a7173df47488514a7abfd997e86778ee9bf8
emawit <arsemaermiyas553@gmail.com>
2024-08-26 05:46:36 -0700

    update

--- calculator.py
+++ calculator.py
@@ -1,1 +1,18 @@
+while True:
+    # taking input from the user
+    num1=int(input("num1: - "))
+    num2=int(input("num2: - "))
+    opreation=input("opreation: - ")
+    # making opreation
+    if opreation=="+":
+        print(num1+num2)
+    elif opreation== "-":
+        print(num1-num2)
+    elif opreation=="*":
+        print(num1*num2)
+    elif opreation=="/":
+        print(num1/num2)
+    else:
+        print("invalid")
 
+    
