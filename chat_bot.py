b1e65c44378060d887aad017f602a568cb1529a4
hlmich <hlmichaelasrat@gmail.com>
2024-08-30 15:43:03 +0300

    push

---
+++ chat_bot.py
@@ -0,0 +1,8 @@
+while True:
+      name = input("name: ")
+      age = input("age: ")
+      phonnumber = input("phonnumber: ")
+      
+
+
+      
---
+++ temp_converter.py
@@ -0,0 +1,3 @@
+celsius=int(input("enter a temprtur :"))
+F = (celsius*5/9)+32
+print("temperature", F)
