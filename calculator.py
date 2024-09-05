628696c3a9a6a085ad2da9a8f84f10e9068823d4
Yared Kebede <135129440+Yareda21@users.noreply.github.com>
2024-09-05 10:34:24 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,15 +1,1 @@
-while True:
-    num1 =input("num1: -")
-    num2 =input("num2: -")
-    operator =input("+, -, /, *: -")
-    if operator =="+":
-        print(num1+num2)
-    elif operator =="-":
-        print(num1-num2)
-    elif operator =="/":
-        print(num1/num2)
-    elif operator =="*":
-        print(num1*num2)
-    else:
-        print("invalid")
 
