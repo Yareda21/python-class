489f6194a76cfdbd1f3b54010d71712af24e717d
ysimon772 <yoramsimon766@gmail.com>
2024-10-14 16:35:02 +0300

    done my work
    guess game &
    temp

---
+++ guess_game.py
@@ -0,0 +1,17 @@
+### 3.	Guess the Number Game: 
+#Implement a simple number guessing game where the computer generates a random number, and the player tries to guess it. Provide hints like "too high" or "too low" after each guess, and keep track of the number of attempts.
+while True:
+
+    number = 7
+    
+    guess = int(input("enter your guess: _"))
+    if number == guess:
+        print("you guessed correct")
+        break
+
+        counter <"4" 
+    elif number != guess: 
+        
+        print("the guess is wrong") 
+        
+                
